92035950182e21a27e3e5806d825961ebfd5a395
Kuldeep Aggarwal <kd.engineer@yahoo.co.in>
2014-12-05 21:30:41 +0530

    initial commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+ruby '2.1.2'
+
+gem 'rails',          '3.2.20'
+gem 'pg',             '0.17.1'
+gem 'jquery-rails',   '~> 3.1.2'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier',     '>= 1.0.3'
+end
+
+group :development do
+  gem 'byebug',       '~> 3.5.1'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmployeeManagement::Application.config.secret_token = '166a2fb6b596e6cd8f543971ecf38f187c442238451c144af570526c2c0fb46319190104fda89e39ca4f998dac42997da59f09a1c640b86b360ec796938c9fda'
